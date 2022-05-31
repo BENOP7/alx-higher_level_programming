faef6819be9058af51006c98b3d1144364ff27fa
benop7 <benjaminpetegnong@gmail.com>
2022-05-31 23:23:16 +0100

    task 1 done - correct logic

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,10 +4,10 @@ number = random.randint(-10000, 10000)
 if number > -1:
     lastdigit = number % 10
 else:
-    lastdigit = (-number) % 10
+    lastdigit = (-number) % 10 * -1
 print('Last digit of {0:d} is {1:d} and'.format(number, lastdigit), end=' ')
 if lastdigit == 0:
-    print('is zero')
+    print('is 0')
 elif lastdigit > 5:
     print('is greater than 5')
 else:
