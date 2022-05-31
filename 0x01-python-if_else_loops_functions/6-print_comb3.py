ffa78d0f4108b0d736327fc20997838a3fba055f
benop7 <benjaminpetegnong@gmail.com>
2022-05-31 23:57:33 +0100

    correct pycodestyle

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for n in range(10):
     for m in range(n + 1, 10):
         if (n == 8):
             print('{:d}{:d}'.format(n, m))
-            break;
+            break
         print('{:d}{:d},'.format(n, m), end=' ')
