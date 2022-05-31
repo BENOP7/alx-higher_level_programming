ee89fee3f68dc0608dcd0fbd32b89c4706a76709
benop7 <benjaminpetegnong@gmail.com>
2022-06-01 00:58:05 +0100

    task 11 - optimised

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -3,7 +3,12 @@
 def pow(a, b):
     i = 0
     r = 1
-    while i < b:
-        r *= a
-        i += 1
+    if b > 0:
+        while i < b:
+            r *= a
+            i += 1
+    if b < 0:
+        while i < b:
+            r /= a
+            i += 1
     return r
