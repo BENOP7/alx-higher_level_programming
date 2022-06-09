68c5a5dc59aa7eb6eadc08deed89f88a532f07e9
Benop <benjaminpetegnong@gmail.com>
2022-06-09 12:50:04 +0000

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+	cpy = []
+	
+	if !matrix:
+		return
+	for row in matrix:
+		cpy.append(list(map(square, row)))
+	return cpy
+
+def square(x):
+	return x*x	
