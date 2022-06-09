5114ca3829a416ec5244bcf7fe734eacd15b6699
benop7 <benjaminpetegnong@gmail.com>
2022-06-09 08:42:22 -0700

    correct pycodestyle errors

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	cpy = []
-	
-	if matrix == None:
-		return
-	for row in matrix:
-		cpy.append(list(map(square, row)))
-	return cpy
+    cpy = []
+    if matrix is None:
+        return
+    for row in matrix:
+        cpy.append(list(map(square, row)))
+    return cpy
+
 
 def square(x):
-	return x*x	
+    return x*x
