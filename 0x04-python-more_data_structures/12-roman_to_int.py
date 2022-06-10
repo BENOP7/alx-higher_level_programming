b825435697a31f8cfa81ec256effbd95c91af8af
benop7 <benjaminpetegnong@gmail.com>
2022-06-09 19:44:58 -0700

    correct pycodestyle errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,26 +5,29 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
-            'M': 1000}
+    roman_dict = {'I': 1, 'V': 5,
+                  'X': 10, 'L': 50,
+                  'C': 100, 'D': 500,
+                  'M': 1000}
 
     roman_string = roman_string.upper()
     res = 0
-    l = len(roman_string)
+    size = len(roman_string)
     i = 0
 
-    while i < l:
+    while i < size:
         if roman_string[i] in roman_dict:
-            if i + 1 < l:
-                if roman_string[i] in 'I' and roman_string[i + 1] in ['V','X']:
+            if i + 1 < size:
+                if roman_string[i] in 'I' and roman_string[i + 1]\
+                        in ['V', 'X']:
                     res += roman_dict[roman_string[i + 1]] - 1
                     i += 2
                 elif roman_string[i] in 'X' and\
                         roman_string[i + 1] in ['L', 'C']:
                     res += roman_dict[roman_string[i + 1]] - 10
                     i += 2
-                elif roman_string[i] in 'C' and roman_string[i + 1] in\
-                ['D', 'M']:
+                elif roman_string[i] in 'C' and roman_string[i + 1]\
+                        in ['D', 'M']:
                     res += roman_dict[roman_string[i + 1]] - 100
                     i += 2
                 else:
