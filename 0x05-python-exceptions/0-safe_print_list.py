ad3a1823e52131c003108fe2f49f953219a59f9e
benop7 <benjaminpetegnong@gmail.com>
2022-06-20 19:59:09 -0700

    #

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,13 @@
 
 def safe_print_list(my_list=[], x=0):
     n = 0
-    try:
-        for i in range(0, x):
+
+    for i in range(0, x):
+        try:
             print('{}'.format(my_list[i]), end='')
             n += 1
-    except IndexError:
-        print()
-        return n
+        except IndexError:
+            print()
+            return n
     print()
     return n
