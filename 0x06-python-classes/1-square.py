f3240dd24bf15806178c8da83a5851db1edecf4d
benop7 <benjaminpetegnong@gmail.com>
2022-06-21 14:27:19 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
+""" This module contains the class Square """
+
+
 class Square:
+    """ Square represents a square model """
     def __init__(self, size):
         self.__size = size
