706256d37a601cc41720432516630145fccca8dc
benop7 <benjaminpetegnong@gmail.com>
2022-06-21 14:44:19 -0700

    task 3 - python_classes

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+""" This module contains the class Square """
+
+
+class Square:
+    """ Square represents a square model """
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Return the area of the square """
+        return size ** 2
