12efcb777e8c94b2c5d2b9d78eed429cc183dfe1
benop7 <benjaminpetegnong@gmail.com>
2022-06-21 17:50:32 -0700

    task 6 - adjusted

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,6 +6,10 @@
 class Square:
     """ Square represents a square model """
     def __init__(self, size=0, position=(0, 0)):
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
         self.__position = position
 
