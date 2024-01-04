6e9f2f71048fa1792d8edee60b42928d8a0051b6
benop7 <benjaminpetegnong@gmail.com>
2024-01-04 11:05:53 +0100

    task 2;

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+if __name__ == "__main__":
+    n = len(argv)
+    print(n - 1, "argument" if n == 2 else "arguments")
+
+    for i in range(1, n):
+        print(str(i) + ': ' + argv[i])
