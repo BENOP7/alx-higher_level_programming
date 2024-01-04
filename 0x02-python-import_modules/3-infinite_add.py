e10f8729ef1917daec7052ccc0adab362d2dd537
benop7 <benjaminpetegnong@gmail.com>
2024-01-04 11:37:12 +0100

    correct task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,10 @@ from sys import argv as numbers
 
 
 if __name__ == '__main__':
-    first = int(numbers[1])
-    second = int(numbers[2])
+    first = 0
+    second = 0
+    if len(numbers) == 3:
+        first = int(numbers[1])
+        second = int(numbers[2])
 
     print(first + second)
