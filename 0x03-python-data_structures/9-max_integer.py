d72ce0cd7240c11e9501a391f408002287f54036
benop7 <benjaminpetegnong@gmail.com>
2024-01-07 23:07:47 +0100

    task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def max_integer(my_list=[]):
+    if len(my_list) < 1:
+        return None
+    if len(my_list) == 1:
+        return my_list[0]
+    largest = my_list[0]
+    for n in my_list[1:]:
+        if n > largest:
+            largest = n
