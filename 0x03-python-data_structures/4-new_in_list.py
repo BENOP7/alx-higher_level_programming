2a66f5339f7e3e5cf5b1221467802d56cc03ca52
benop7 <benjaminpetegnong@gmail.com>
2024-01-07 15:00:26 +0100

    task 4 - fix indentation error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 
 
 def new_in_list(my_list, idx, element):
-    return (my_list[:idx] + [element]\
-        + my_list[(idx + 1):] if idx != len(my_list) - 1 else [])
+    return (my_list[:idx] + [element]
+            + my_list[(idx + 1):] if idx != len(my_list) - 1 else [])
