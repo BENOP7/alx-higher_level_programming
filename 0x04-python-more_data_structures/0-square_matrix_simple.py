1700aa6a0d68e4520bf73d1e84a297a5e809e255
benop7 <benjaminpetegnong@gmail.com>
2024-01-10 17:18:59 +0100

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,16 @@
 
 
 def square_matrix_simple(matrix=[]):
-    return [num * num for num in matrix]
+    return [[num ** 2 for num in row] for row in matrix]
+
+
+if __name__ == '__main__':
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+    new_matrix = square_matrix_simple(matrix)
+    print(new_matrix)
+    print(matrix)
