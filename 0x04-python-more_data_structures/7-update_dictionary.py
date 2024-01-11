a2327b668d4060ab6bb55fdf23805e68a643535e
benop7 <benjaminpetegnong@gmail.com>
2024-01-11 10:06:47 +0100

    task 7 - correction

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,7 @@
 
 
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is None or key is None:
+    if a_dictionary is None:
         return
     a_dictionary[key] = value
+    return a_dictionary
