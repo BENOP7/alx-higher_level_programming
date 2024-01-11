c1d95ec1203dec935721a8548e9ea2a3a1c3aeff
benop7 <benjaminpetegnong@gmail.com>
2024-01-11 07:19:53 +0100

    task 2

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    total = 0
+    if my_list is None:
+        return total
+    number_set = set(my_list)
+    for n in my_list:
+        total += n
+    return total
+
+
+if __name__ == '__main__':
+    uniq_add([1, 2, 3])
