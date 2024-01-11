69aeed7038faeb8253ab6bb64124733d75b044bf
benop7 <benjaminpetegnong@gmail.com>
2024-01-11 07:38:06 +0100

    task4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,4 @@
 def only_diff_elements(set_1, set_2):
     if set_1 is None or set_2 is None:
         return set()
-    return set_1.difference(set_2)
+    return set_1.difference(set_2).union(set_2.difference(set_1))
