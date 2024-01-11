416aec57f8527549982fa64c55ef2bc10b387a4a
benop7 <benjaminpetegnong@gmail.com>
2024-01-11 07:23:53 +0100

    tas 2 - correct

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -6,7 +6,7 @@ def uniq_add(my_list=[]):
     if my_list is None:
         return total
     number_set = set(my_list)
-    for n in my_list:
+    for n in number_set:
         total += n
     return total
 
