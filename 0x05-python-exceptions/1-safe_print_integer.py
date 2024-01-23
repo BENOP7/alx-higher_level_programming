bf9dd89e3ee69789af0b7ee93a15839df28d9043
benop7 <benjaminpetegnong@gmail.com>
2024-01-23 04:24:54 +0100

    task 1 - correct pycodestyle

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
     try:
         print("{:d}".format(int(value)))
         return True
-    except:
+    except TypeError or ValueError:
         return False
