91599ec43d5d5a077f17b787d086b6301c52bc37
benop7 <benjaminpetegnong@gmail.com>
2024-01-23 03:52:28 +0100

    update task

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    r = None
+    try:
+        r = fct(*args)
+        return r
+    except Exception as e:
+        print('Exception: {}'.format(e),
+              file=sys.stderr)
+        return r
