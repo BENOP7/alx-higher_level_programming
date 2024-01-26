d3eace238f0f029dfd3bcb792d7392b680bd8195
benop7 <benjaminpetegnong@gmail.com>
2024-01-26 12:11:52 +0100

    init subdirectory

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+This module contains the add_integer() function,
+which adds two numbers and returns their sum
+'''
+
+def add_integer(a, b=98):
+    '''
+    Adds two integers and returns the sum
+    '''
+    if type(a) not in [int, float]:
+        raise TypeError('a must be an integer')
+    if type(b) not in [int, float]:
+        raise TypeError('b must be an integer')
+    a = int(a)
+    b = int(b)
+    return a + b
