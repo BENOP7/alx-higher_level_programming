47a783f70564577fa4f62685ac9edc8f81e62738
benop7 <benjaminpetegnong@gmail.com>
2024-02-06 00:09:18 +0100

    tssk 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 '''
-    This module contains ``Rectangle`` class that 
+    This module contains ``Rectangle`` class that
     inherits from the class ``BaseGeometry``
 '''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -10,6 +12,9 @@ class Rectangle(BaseGeometry):
     A quadrilateral (having four sides)
     '''
     def __init__(self, width, height):
+        '''
+        This is the constructor of the rectangle class
+        '''
         self.integer_validator('width', width)
         self.__width = width
         self.integer_validator('height', height)
