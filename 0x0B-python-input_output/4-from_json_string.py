e268c3cf30fb6114b2e27fbca33f8721701a7a48
benop7 <benjaminpetegnong@gmail.com>
2024-02-07 02:32:46 +0100

    task 4

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+This module contains the function ``from_json_string``
+'''
+
+
+def from_json_string(my_str):
+    '''
+    returns an object represented by a JSON string
+    '''
+    return json.loads(my_str)
