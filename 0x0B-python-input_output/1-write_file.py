6b0964f3230174b6ca20c2e6206d48b34b8611af
benop7 <benjaminpetegnong@gmail.com>
2024-02-07 02:05:45 +0100

    task 1 - add return

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,6 +8,8 @@ def write_file(filename='', text=''):
     '''
     Writes the content of a string to the text file
     '''
+    count = 0
     with open(filename, 'w', encoding='utf-8') as f:
         if type(text) is str:
-            f.write(text)
+            count = f.write(text)
+    return count
