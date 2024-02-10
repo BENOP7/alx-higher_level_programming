123db4133217ba1769133f5dcdc0a7118a44ea0c
benop7 <benjaminpetegnong@gmail.com>
2024-02-10 12:17:09 +0100

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+This module contains a ``Square`` class
+That inherits from ``Rectangle``
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    This class implements a square plane with a side
+    It inherits the ``Rectangle`` class
+    '''
+    def __init__(self, size):
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        '''
+        Calculates the area of the square and returns it
+        '''
+        return self.__size ** 2
+
+    def __str__(self):
+        return f'[{self.__class__.__name__}] {self.__size}/{self.__size}'
