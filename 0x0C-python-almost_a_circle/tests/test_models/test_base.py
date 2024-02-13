a11fe9330b689c92031a8860885b18a540e4aea7
benop7 <benjaminpetegnong@gmail.com>
2024-02-13 10:16:07 +0100

    correct test errors

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 '''
 This contains the test class for the ``Base`` class
 
@@ -11,6 +12,9 @@ class TestBase(unittest.TestCase):
     Unit testing the base class
     '''
     def test_an_instant_no_arg(self):
+        '''
+        test instantiation with no argument
+        '''
         b = Base()
         self.assertEqual(b.id, 1)
 
@@ -22,33 +26,33 @@ class TestBase(unittest.TestCase):
         b2 = Base(0)
         cls = b1.__class__
         self.assertEqual(cls.__name__, 'Base')
-        self.assertEqual(b1.id, 2)
+        self.assertEqual(b1.id, 3)
         self.assertEqual(b2.id, 0)
         self.assertRaises(TypeError, Base, None, 2)
 
     def test_nb_objects(self):
-	'''
+        '''
         running test
         '''
         self.assertEqual(hasattr(Base, '__nb_objects'), False)
 
 
     def test_given_id(self):
-	'''
+        '''
         running test
         '''
         b = Base(13)
         self.assertEqual(b.id, 13)
 
     def test_negative_id(self):
-	'''
+        '''
         running test
         '''
         b = Base(-2)
         self.assertEqual(b.id, -2)
 
     def test_id(self):
-	'''
+        '''
         running test
         '''
         self.assertEqual(hasattr(Base(), 'id'), True)
