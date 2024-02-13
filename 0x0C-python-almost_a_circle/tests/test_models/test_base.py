b4179f65ea51d199e22e2b6b360c2055ee71f347
benop7 <benjaminpetegnong@gmail.com>
2024-02-13 09:48:47 +0100

    add docs

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -12,6 +12,9 @@ class TestBase(unittest.TestCase):
     Unit testing the base class
     '''
     def test_instant(self):
+        '''
+        running test
+        '''
         b1 = Base()
         b2 = Base(0)
         cls = b1.__class__
@@ -21,18 +24,30 @@ class TestBase(unittest.TestCase):
         self.assertRaises(TypeError, Base, None, 2)
 
     def test_nb_objects(self):
+	'''
+        running test
+        '''
         self.assertEqual(hasattr(Base, '__nb_objects'), False)
 
 
     def test_given_id(self):
+	'''
+        running test
+        '''
         b = Base(13)
         self.assertEqual(b.id, 13)
 
     def test_negative_id(self):
+	'''
+        running test
+        '''
         b = Base(-2)
         self.assertEqual(b.id, -2)
 
     def test_id(self):
+	'''
+        running test
+        '''
         self.assertEqual(hasattr(Base(), 'id'), True)
 
 
