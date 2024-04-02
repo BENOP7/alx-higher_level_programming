409c4c9a4dcf38d08c9aaafea7edaf1d2da972b9
benop7 <benjaminpetegnong@gmail.com>
2024-04-02 12:38:48 +0100

    correct script

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- count the number of scores
-SELECT score, COUNT(*) as number FROM second_table ORDER BY number;
+SELECT score, COUNT(*) as number FROM second_table GROUP BY score ORDER BY number DESC;
